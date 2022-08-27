d67cfae53e26130df211fdbd9f064035e102b0ac
nat <natphrak97@gmail.com>
2022-08-27 14:18:32 -0500

    Added function that randomly returns Rock, Paper, or Scissors

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 function getComputerChoice() {
-    function getRandomInt(max) {
-        return Math.floor(Math.random() * max);
+    function createHand() {
+        function getRandomInt(max) {
+            return Math.floor(Math.random() * max);
+        }
+        let randomInt = getRandomInt(3);
+        let computerHand;
+        switch(randomInt) {
+            case 0:
+                computerHand = "Rock";
+                break;
+            case 1:
+                computerHand = "Paper";
+                break;
+            case 2:
+                computerHand = "Scissors";
+                break;
+        }
+        return computerHand;
     }
 
-    return getRandomInt(3);
+    return createHand();
 
 }
 
