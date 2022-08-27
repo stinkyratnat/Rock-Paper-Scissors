34bfb408a31d44d41215325e0925888019b45465
nat <natphrak97@gmail.com>
2022-08-27 14:22:24 -0500

    Remove redundant function

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 function getComputerChoice() {
-    function createHand() {
-        function getRandomInt(max) {
-            return Math.floor(Math.random() * max);
-        }
-        let randomInt = getRandomInt(3);
-        let computerHand;
-        switch(randomInt) {
-            case 0:
-                computerHand = "Rock";
-                break;
-            case 1:
-                computerHand = "Paper";
-                break;
-            case 2:
-                computerHand = "Scissors";
-                break;
-        }
-        return computerHand;
+    function getRandomInt(max) {
+        return Math.floor(Math.random() * max);
     }
-
-    return createHand();
-
+    let randomInt = getRandomInt(3);
+    let computerHand;
+    switch(randomInt) {
+        case 0:
+            computerHand = "Rock";
+            break;
+        case 1:
+            computerHand = "Paper";
+            break;
+        case 2:
+            computerHand = "Scissors";
+            break;
+    }
+    return computerHand;
 }
 
 console.log(getComputerChoice());
+
+let getPlayerChoice = prompt("test");
+let playerSelection = getPlayerChoice.toLowerCase();
+
+console.log(playerSelection);
